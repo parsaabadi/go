ad64a117962cb65ad53681c05b554802b8852b15
amc1999 <29126882+amc1999@users.noreply.github.com>
2021-06-12 00:33:42 -0400

    fix OzProj run .bat: incorrect connection string

--- etc/scripts/oms-OzProj-run.bat
+++ etc/scripts/oms-OzProj-run.bat
@@ -10,4 +10,4 @@ rem OM_T_BIN_DIR
 set OM_%OM_T_MODEL_NAME%=../..
 cd .\..\%OM_T_MODEL_NAME%\ompp\bin
 
-..\..\..\bin\%OM_T_EXE_STEM% -OpenM.Database Database="..\..\..\bin\%OM_T_MODEL_NAME%.sqlite;OpenMode=ReadWrite;Timeout=86400;" %*
+..\..\..\bin\%OM_T_EXE_STEM% -OpenM.Database "Database=..\..\..\bin\%OM_T_MODEL_NAME%.sqlite;OpenMode=ReadWrite;Timeout=86400;" %*
